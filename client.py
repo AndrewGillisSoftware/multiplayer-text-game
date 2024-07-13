0123c50e3b020f0a356860d5f05b537be840219e
AndrewGillisSoftware <gillisvandrew@gmail.com>
2024-07-13 13:22:37 -0400

    Half way through mail system development. Still in pickle phase

--- client.py
+++ client.py
@@ -1,11 +1,36 @@
 from comm_utils import *
+from shared_classes import *
 
 SERVER = "192.168.99.41"
+IP = socket.gethostbyname(socket.gethostname())
 
-ct = ClientTransport(None)
+def parse_command(command):
+    return
+
+ct = ClientTransport(SERVER, None)
 ct.connect(SERVER)
 
-response = ct.send("Hello world")
+while True:
+    command = input("Enter Command: ")
 
-ct.disconnect()
+    # /msg_IP_
+    parts = command.split('_')
 
+    match parts[0]:
+        case "/msg":
+            match parts[1]:
+                case "BROADCAST":
+                    print("NOT SUPPORTED")
+                case _:
+                    # must be IP
+                    ip_to_send_to = parts[1]
+                    message_to_send = parts[2]
+                    sms = ParcelSmsString(str(IP), message_to_send)
+                    response = ct.send_parcel(ip_to_send_to, sms)
+        case "/ping":
+            response = ct.send_parcel(SERVER, ParcelPing())
+                    
+        case "/disconnect":
+            ct.disconnect()
+        case _:
+            print("INVALID COMMAND")
--- comm_utils.py
+++ comm_utils.py
@@ -1,19 +1,68 @@
 import socket
 import threading
+import datetime
+import pickle
+from shared_classes import *
 
 HEADER_BYTES = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!@#$%^&*()!DISCONNECT!DISCONNECT_NOW!@#$%^&*()" # Payload has a very low chance to look like this
+EMPTY_PARCEL = "!NETWORK_TRANSPORT_EMPTY!"
+DISCONNECT_MESSAGE = "!NETWORK_TRANSPORT_DISCONNECTED!" # Payload has a very low chance to look like this
+
+class MailParcel:
+    def __init__(self, from_address, to_address, message):
+        self.time_stamp = datetime.datetime.now()
+        self.from_address = from_address
+        self.to_address = to_address
+        self.message = message
+        return
+    
+    def get_message(self):
+        return pickle.loads(self.message)
+
+class MailBox:
+    box = []
+
+    def __init__(self, server_address, address):
+        self.server_address = server_address
+        self.address = address
+        return
+
+    def add_parcel(self, parcel):
+        self.box.append(parcel)
+        return
+
+    def get_next_parcel(self) -> MailParcel:
+        if len(self.box) == 0:
+            # Return Empty Parcel
+            return MailParcel(self.server_address, self.address, EMPTY_PARCEL)
+
+        return self.box.pop()
 
 class ClientTransport:
-    def __init__(self, handle_server):
+    def __init__(self, server_address, handle_server):
+        self.server_address = server_address
+        self.client_address = socket.gethostbyname(socket.gethostname())
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((server_address, PORT))
+        return
 
-    def connect(self, address):
-        self.client.connect((address, PORT))
+    # If IP is server the message is for the server
+    def send_parcel(self, to_address, any_object) -> MailParcel:
+        # Create Parcel
+        parcel = MailParcel(self.client_address, to_address, pickle.dumps(any_object))
 
+        # Pickle Parcel
+        pickled_parcel = pickle.dumps(parcel)
+
+        # Get Size of Pickle Parcel
+        
+
+
+    """
     def send(self, msg):
+        # Send
         message = msg.encode(FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
@@ -21,45 +70,72 @@ class ClientTransport:
         self.client.send(send_length)
         self.client.send(message)
 
+        # Response
+
+        # Get addr_length payload
+        addr_length = self.client.recv(HEADER_BYTES).decode(FORMAT)
+        addr_length = int(addr_length)
+
+        # Get Address
+        addr = self.client.recv(addr_length).decode(FORMAT)
+
+        # Get msg_length of payload
+        msg_length = self.client.recv(HEADER_BYTES).decode(FORMAT)
+        msg_length = int(msg_length)
+
+        # Get response message
+        response = self.client.recv(msg_length).decode(FORMAT)
+    
+        print(f"[Received Parcel of Mail from {addr}] {response}")
+        return response
+    """
+
     def disconnect(self):
-        self.send(DISCONNECT_MESSAGE)
+        self.send_parcel(self.server_address, ParcelDisconnect)
+        return
 
 class ServerTransport:
-    # IPs of connected clients
-    active_clients = []
 
-    # (address, message) tuples that get sent to clients when clients ping the server
-    # Once sent the tuple is removed from the mail box. Messages are sent based on when they were added to
-    # the mailbox
-    client_mailbox_queue = []
+    mail_boxes = []
 
     def __init__(self, handle_client):
         self.handle_client = handle_client
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address = socket.gethostbyname(socket.gethostname())
         self.server.bind((self.address, PORT))
+        return
+    
+    def __get_mailbox(self, address):
+        # Ensure box does not already exist
+        for box in self.mail_boxes:
+            if box.address == address:
+                return box
+        return None
 
     def __register_client(self, address):
-        # Add Client to list of active clients
-        self.active_clients.append(address)
+        # Ensure box does not already exist
+        box = self.__get_mailbox(address)
+        if box:
+            return
+        # Add client to list of active clients by creating a mailbox
+        # self.address = Server Address
+        self.mail_boxes.append(MailBox(self.address, address))
+        return
 
     def __deregister_client(self, address):
-        # Remove Client from active clients
-        self.active_clients.remove(address)
-
-        # Clean Mailbox for given client
-        for client_message in self.client_mailbox_queue:
-            if client_message[0] == address:
-                self.client_mailbox_queue.remove(client_message)
-
-    def __get_client_mail(self, address):
-        mail = []
-        # Clean Mailbox for given client
-        for client_message in self.client_mailbox_queue:
-            if client_message[0] == address:
-                mail.append(client_message[1])
-                self.client_mailbox_queue.remove(client_message)
-        return mail
+        # Remove mail box
+        box = self.__get_mailbox(address)
+        if box:
+            self.mail_boxes.remove(box)
+        return
+    
+    def __send_to_client_proto(self, conn, msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER_BYTES - len(send_length))
+        conn.send(send_length)
+        conn.send(message)
 
     def __handle_client_proto(self, conn, addr):
         print(f"[NEW CONNECTION] {addr} connected.")
@@ -70,6 +146,7 @@ class ServerTransport:
         while connected:
             msg_length = conn.recv(HEADER_BYTES).decode(FORMAT)
             if msg_length:
+                # Get msg_length payload
                 msg_length = int(msg_length)
                 msg = conn.recv(msg_length).decode(FORMAT)
                 if msg == DISCONNECT_MESSAGE:
@@ -77,13 +154,18 @@ class ServerTransport:
                 
                 print(f"[{addr}] {msg}")
 
+                # Server reacts to clients hook
                 if self.handle_client != None:
                     self.handle_client(msg)
 
-                client_mail = self.__get_client_mail(addr)
-
-                for message in client_mail:
-                    print(f"[Sending Mail to {addr}] {message}")
+                # Send address
+                self.__send_to_client_proto(conn, addr)
+                
+                # Send next parcel to client
+                box = self.__get_mailbox(addr)
+                next_parcel_for_client = box.get_next_parcel()
+                print(f"[Sending Next Parcel of Mail to {addr}] {next_parcel_for_client}")
+                self.__send_to_client_proto(conn, next_parcel_for_client)
         
         self.__deregister_client(addr)
         conn.close()
@@ -99,11 +181,6 @@ class ServerTransport:
             thread.start()
             print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
     
-    def send_to_client(self, client_address, msg):
-        self.client_mailbox_queue.append((client_address, msg))
-
-    def broadcast(self, msg):
-        # For each client address
-        for client_address in self.active_clients:
-            # Add Message to Client's mailbox
-            self.send_to_client(client_address, msg)
+    def send_to_client(self, from_address, to_address, msg):
+        mail = MailParcel(from_address, to_address, msg)
+        self.client_mailbox_queue.append(mail)
