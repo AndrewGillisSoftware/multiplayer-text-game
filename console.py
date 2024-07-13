19b3269e84a1f5944d0351d191619402f8bed81d
AndrewGillisSoftware <gillisvandrew@gmail.com>
2024-07-12 23:14:55 -0400

    Created basic console and mocked out display to integrate with Matts design

---
+++ console.py
@@ -0,0 +1,26 @@
+from display import *
+CURSOR = ">"
+
+def set_cursor():
+    set_display_at(0, DISPLAY_ROWS - 1, CURSOR)
+
+def console_response(input):
+    return "? SYNTAX ERROR"
+
+def console_prompt():
+    set_cursor()
+    print_display()
+
+    set_cursor()
+    user_input = input(">")
+    set_display_at(1, DISPLAY_ROWS - 1, user_input)
+    print_display()
+    shift_up_display(1)
+
+    response = console_response(user_input)
+    set_display_at(0, DISPLAY_ROWS - 1, response)
+    print_display()
+    shift_up_display(1)
+
+while True:
+    console_prompt()
---
+++ display.py
@@ -0,0 +1,35 @@
+import os
+
+EMPTY_CHARACTER = "."
+DISPLAY_ROWS, DISPLAY_COLS = (24, 40)
+
+# Correctly initialize the display matrix with separate lists for each row
+display = [[EMPTY_CHARACTER] * DISPLAY_COLS for _ in range(DISPLAY_ROWS)]
+
+def print_display():
+    cls()
+    for row in display:
+        for character in row:
+            print(str(character) + " ", end="")
+        print()
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def set_display_at(posx, posy, string):
+    for i in range(len(string)):
+        display[posy][posx + i] = string[i]
+
+def shift_up_display(shift_number):
+    if shift_number >= DISPLAY_ROWS:
+        # If x is greater than or equal to the number of rows, clear the entire display
+        for i in range(DISPLAY_ROWS):
+            display[i] = [EMPTY_CHARACTER] * DISPLAY_COLS
+    else:
+        # Shift the display up by x rows
+        for i in range(shift_number, DISPLAY_ROWS):
+            display[i - shift_number] = display[i]
+        # Clear the last x rows
+        for i in range(DISPLAY_ROWS - shift_number, DISPLAY_ROWS):
+            display[i] = [EMPTY_CHARACTER] * DISPLAY_COLS
+
